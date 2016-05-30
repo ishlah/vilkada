78e5a097084bbb11cb1882dc470815fe1e1e0c35
Muhamad Ishlah <nurul.ishlah@gmail.com>
2016-05-30 16:09:57 +1000

    Added doughnut chart with dummy data

--- src/components/ResultSplash.js
+++ src/components/ResultSplash.js
@@ -1,30 +1,56 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Doughnut } from 'react-chartjs';
+
+const DoughnutChart = Doughnut;
 
 class ResultSplash extends Component {
   render() {
+
+    let chartData = [
+      {
+        color: '#2199e8',
+        highlight: '#2199e6',
+        label: 'Memilih',
+        value: 85434
+      },
+      {
+        color: '#fff',
+        highlight: '#eee',
+        label: 'Tidak memilih',
+        value: 200000 - 85434
+      }
+    ];
+
     return (
-      <div className="result-splash">
+      <div className="result-splash" 
+        style={{backgroundImage: 'linear-gradient(to bottom, rgba(0,0,0,0.5), rgba(0,0,0,0.8)), url(/images/bogor_palace_front.jpg)'}}>
         <div className="splash-infographic">
           <div className="splash-header">
             <h1 className="splash-title">{this.props.selectedRegion}</h1>
             <p>{Object.keys(this.props.recapitulation).length} Daerah Pemilihan</p>
           </div>
           <div className="splash-body row">
-            <div className="small-4 columns">
-              <div>dough chart here</div>
-              <p className="text-info">
-                85.434 dari 200.000 jiwa <br />
-                menggunakan hak pilihnya
-              </p>
+            <div className="small-12 medium-4 large-4 columns">
+              <DoughnutChart data={chartData} options={{responsive: true}} />
+              <div className="text-info">
+                <h2>85.434</h2>
+                <p>dari 200.000 jiwa<br/>menggunakan hak pilihnya</p>
+              </div>
             </div>
-            <div className="small-4 columns">
-              <div>dough chart here</div>
-              <p className="text-info">85.434 dari 200.000 suara sah</p>
+            <div className="small-12 medium-4 large-4 columns">
+              <DoughnutChart data={chartData} options={{responsive: true}} />
+              <div className="text-info">
+                <h2>85.434</h2>
+                <p>dari 200.000 suara sah</p>
+              </div>
             </div>
-            <div className="small-4 columns">
-              <div>dough chart here</div>
-              <p className="text-info">Kandidat no urut 1 menang dengan 58.000 suara</p>
+            <div className="small-12 medium-4 large-4 columns">
+              <DoughnutChart data={chartData} options={{responsive: true}} />
+              <div className="text-info">
+                <h2>85.434 <small>suara</small></h2>
+                <p>diperoleh kandidat no urut 1<br/>(Pemenang)</p>
+              </div>
             </div>
           </div>
         </div>
